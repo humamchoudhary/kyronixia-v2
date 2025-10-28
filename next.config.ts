5b296c21c57316da1010f4f13a94a65f2f01cfd3
humamchoudhary <humamchoudhary@gmail.com>
2025-10-28 17:11:17 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  // If using custom image domains
+  images: {
+    domains: ["kyronixia.com"],
+    // Or use remotePatterns for Next.js 13+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
