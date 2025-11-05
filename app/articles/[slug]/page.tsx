d012fb2b2b8e9e012fd9bdb83da07431d2a96487
humamchoudhary <humamchoudhary@gmail.com>
2025-11-05 23:25:33 +0500

    update

---
+++ app/articles/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import "./articles.css";
+import { getArticleData } from "@/lib/articles";
+import { ArrowLeft } from "lucide-react";
+
+const Article = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = await params;
+  const articleData = await getArticleData(slug);
+
+  return (
+    <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
+      <div className="flex justify-between font-poppins">
+        <Link href={"/"} className="flex flex-row gap-1 place-items-center">
+          <ArrowLeft size={20} />
+          <p>back to home</p>
+        </Link>
+        <p>{articleData.date.toString()}</p>
+      </div>
+      <article
+        className="article"
+        dangerouslySetInnerHTML={{ __html: articleData.contentHtml }}
+      />
+    </section>
+  );
+};
+
+export default Article;
